4f5a1d8f9bf696bedb12f02e999256e77d3f7609
m001 <m001@benner.com.br>
2018-07-20 11:44:47 -0300

    Ajustando Form novamente

--- T0/ContaBanco/Form1.Designer.cs
+++ T0/ContaBanco/Form1.Designer.cs
@@ -159,7 +159,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(607, 441);
+            this.ClientSize = new System.Drawing.Size(205, 286);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.destinoDaTransferencia);
             this.Controls.Add(this.comboContas);
@@ -174,6 +174,7 @@
             this.Controls.Add(this.textoSaldo);
             this.Controls.Add(this.textoTitular);
             this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
