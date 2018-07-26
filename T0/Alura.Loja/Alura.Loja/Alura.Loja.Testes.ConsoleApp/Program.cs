7e148f9d106eef47ea93aa95dedc969beceb6e3d
m001 <m001@benner.com.br>
2018-07-26 08:01:43 -0300

    Recuperando com Entity

---
+++ T0/Alura.Loja/Alura.Loja/Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Alura.Loja.Testes.ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // GravarUsandoAdoNet();
+            // GravarUsandoEntity();
+            RecuperaProduto();
+        }
+
+        private static void RecuperaProduto()
+        {
+            using (var repo = new LojaContext())
+            {
+                IList<Produto> produtos = repo.Produtos.ToList();
+               
+                foreach(var item in produtos)
+                {
+                    Console.WriteLine(item.Nome);
+                }
+            }
+        }
+
+        private static void GravarUsandoEntity()
+        {
+            Produto p = new Produto();
+            p.Nome = "Harry Potter e a Ordem da Fênix";
+            p.Categoria = "Livros";
+            p.Preco = 19.89;
+
+            Produto p2 = new Produto();
+            p2.Nome = "O Simbolo perdido";
+            p2.Categoria = "Livros";
+            p2.Preco = 10.00;
+
+            using (var Contexto = new LojaContext())
+            {
+                Contexto.Produtos.AddRange(p, p2);
+                Contexto.SaveChanges();
+            }
+        }
+
+        private static void GravarUsandoAdoNet()
+        {
+            Produto p = new Produto();
+            p.Nome = "Harry Potter e a Ordem da Fênix";
+            p.Categoria = "Livros";
+            p.Preco = 19.89;
+
+            using (var repo = new ProdutoDAO())
+            {
+                repo.Adicionar(p);
+            }
+        }
+    }
+}
