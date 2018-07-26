538378de1e9b0e3c483c1ca7d41a3c74fb175c90
m001 <m001@benner.com.br>
2018-07-26 10:15:28 -0300

    Adicionando Produto e Verificando Mudança no ChangeTracker

--- T0/Alura.Loja/Alura.Loja/Alura.Loja.Testes.ConsoleApp/Produto.cs
+++ T0/Alura.Loja/Alura.Loja/Alura.Loja.Testes.ConsoleApp/Produto.cs
@@ -8,7 +8,7 @@
         public double Preco { get; internal set; }
         public override string ToString()
         {
-            return "Produto: " + this.Nome;
+            return $"Produto: {this.Id}, {this.Nome}, {this.Categoria}, {this.Preco}";
         }
     }
 }
