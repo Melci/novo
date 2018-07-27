fd412d38df57e17d52b7a5176655271829eb34d5
m001 <m001@benner.com.br>
2018-07-27 09:39:59 -0300

    Criando Tabela compra

--- T0/Alura.Loja/Alura.Loja/Alura.Loja.Testes.ConsoleApp/LojaContext.cs
+++ T0/Alura.Loja/Alura.Loja/Alura.Loja.Testes.ConsoleApp/LojaContext.cs
@@ -6,7 +6,7 @@ namespace Alura.Loja.Testes.ConsoleApp
     internal class LojaContext : DbContext
     {
         public DbSet<Produto> Produtos { get; set; }
-
+        public DbSet<compra> Compras { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer("Server=(localdb)\\mssqllocaldb;Database=LojaDB;Trusted_Connection=true;");
--- T0/Alura.Loja/Alura.Loja/Alura.Loja.Testes.ConsoleApp/Program.cs
+++ T0/Alura.Loja/Alura.Loja/Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -11,44 +11,16 @@ namespace Alura.Loja.Testes.ConsoleApp
     {
         static void Main(string[] args)
         {
-            using (var contexto = new LojaContext())
-            {
-                var produtos = contexto.Produtos.ToList();
+            var paofrances = new Produto();
+            paofrances.Nome = "Pão Francês";
+            paofrances.PrecoUnitario = 0.40;
+            paofrances.Unidade = "Unidade";
+            paofrances.Categoria = "Padaria";
 
-                ExibeEntries(contexto.ChangeTracker.Entries());
-
-                /*var novoProduto = new Produto()
-                {
-                    Nome = "Desinfetante",
-                    Categoria = "limpeza",
-                    Preco = 2.99
-                };
-                contexto.Produtos.Add(novoProduto);*/
-
-                var p1 = produtos.First();
-                contexto.Produtos.Remove(p1);
-
-                ExibeEntries(contexto.ChangeTracker.Entries());
-
-                Console.WriteLine("---------------");
-                foreach (var e in contexto.ChangeTracker.Entries())
-                {
-                    Console.WriteLine(e.State);
-                }
-
-                contexto.SaveChanges();
-
-                ExibeEntries(contexto.ChangeTracker.Entries());
-            }
-        }
-
-        private static void ExibeEntries(IEnumerable<EntityEntry> entries)
-        {
-            Console.WriteLine("---------------");
-            foreach (var e in entries)
-            {
-                Console.WriteLine(e.Entity.ToString() + " - " + e.State);
-            }
+            var compra = new compra();
+            compra.Quantidade = 6;
+            compra.Produto = paofrances;
+            compra.Preco = paofrances.PrecoUnitario * compra.Quantidade;
         }
     }
 }
