bc2d5b6a6c15eff5f811b22061bd15834a1f8057
m001 <m001@benner.com.br>
2018-07-27 11:54:20 -0300

    adicionando produto na promoção

--- T0/Alura.Loja/Alura.Loja/Alura.Loja.Testes.ConsoleApp/Program.cs
+++ T0/Alura.Loja/Alura.Loja/Alura.Loja.Testes.ConsoleApp/Program.cs
@@ -11,22 +11,26 @@ namespace Alura.Loja.Testes.ConsoleApp
     {
         static void Main(string[] args)
         {
+            var p1 = new Produto() { Nome = "Suco de Laranja", Categoria = "Bebidas",   PrecoUnitario = 8.79,  Unidade = "Litros" };
+            var p2 = new Produto() { Nome = "Café",            Categoria = "Bebidas",   PrecoUnitario = 12.45, Unidade = "Gramas" };
+            var p3 = new Produto() { Nome = "Macarrão",        Categoria = "Alimentos", PrecoUnitario = 4.23,  Unidade = "Gramas" };
+
+
             var promocaoDePascoa = new Promocao();
             promocaoDePascoa.Descricao = "Pascoa Feliz";
             promocaoDePascoa.DataInicio = DateTime.Now;
             promocaoDePascoa.DataTermino = DateTime.Now.AddMonths(3);
 
-           /* promocaoDePascoa.Produtos.Add(new PromocaoProduto());
-            promocaoDePascoa.Produtos.Add(new PromocaoProduto());
-            promocaoDePascoa.Produtos.Add(new PromocaoProduto());*/
-
+            promocaoDePascoa.IncluiProduto(p1);
+            promocaoDePascoa.IncluiProduto(p2);
+            promocaoDePascoa.IncluiProduto(p3);
 
             using (var contexto = new LojaContext())
             {
-              
+
             }
 
-        }
+        } 
         private static void ExibeEntries(IEnumerable<EntityEntry> entries)
         {
             foreach (var e in entries)
